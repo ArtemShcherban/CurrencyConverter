73fd8d844de7d5a1a32bd45c6318578ccef65048
Artem Shcherban <artshcherban@gmail.com>
2022-07-17 16:59:58 +0200

    Three ellipse were created

---
+++ CurrencyConverter/Constraints.swift
@@ -0,0 +1,12 @@
+//
+//  Constraints.swift
+//  CurrencyConverter
+//
+//  Created by Artem Shcherban on 17.07.2022.
+//
+
+import UIKit
+
+let regularConstraints: [NSLayoutConstraint] = []
+let compactConstraints: [NSLayoutConstraint] = []
+let sharedConstraints: [NSLayoutConstraint] = []
